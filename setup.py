e5cef98a26bc78a7e7fab48479a45976ce12393e
vinayak-100ni <vinayaksoni977001@gmail.com>
2026-05-18 15:30:27 +0530

    adding env

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vinayak",
+    author_email="vinayaksoni97700@gmail.com",
+    packages=find_packages()
+)
